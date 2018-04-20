6b19aec7b1b222708d3157f969578874d5534eb6
Asia Antczak <aasiaantczak@gmail.com>
2018-04-20 13:05:22 +0100

    report broken bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,11 @@ describe Bike do
   it { is_expected.to respond_to :working? }
 
   it { is_expected.to respond_to :report_broken}
+
+  describe "#report_broken" do
+    it "reports broken bike" do
+      subject.report_broken
+      expect(subject).not_to be_working
+    end
+  end
 end
